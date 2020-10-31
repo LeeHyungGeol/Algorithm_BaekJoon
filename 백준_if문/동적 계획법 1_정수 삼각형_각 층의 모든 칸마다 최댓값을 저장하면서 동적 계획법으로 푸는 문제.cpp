9dfb65a7e73f6084c3b74ad20fed8534f28dd0b7
LeeHyungGeol <rjf1138@naver.com>
2020-11-01 00:03:09 +0900

    동적 계획법(Dynamic Programming)

--- 백준_if문/동적 계획법 1_정수 삼각형_각 층의 모든 칸마다 최댓값을 저장하면서 동적 계획법으로 푸는 문제.cpp
+++ 백준_if문/동적 계획법 1_정수 삼각형_각 층의 모든 칸마다 최댓값을 저장하면서 동적 계획법으로 푸는 문제.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 //각 층의 모든 칸마다 
 //최댓값을 저장하면서 동적 계획법으로 푸는 문제
-int MAX[501] = { 0 };
-int N;
-int integerTriangle(int sum, int** arr);
 
 int main() {
 
-	int sum = 0;
+	int N, result = 0, dp[501][501] = { 0 };
 	cin >> N;
-	int** arr = new int* [N + 1];
-	for (int i = 1; i <= N; ++i) {
-		arr[i] = new int[i];
-	}
 	for (int i = 1; i <= N; ++i) {
 		for (int j = 1; j <= i; ++j) {
-			cin >> arr[i][j];
+			cin >> dp[i][j];
 		}
-	}
-
-	if (N == 1) {
-		cout << arr[1][1] << '\n';
-		return 0;
-	}
-	for (int i = 1; i <= N; ++i) {
-		MAX[N]=
 	}
 	/*for (int i = 1; i <= N; ++i) {
 		for (int j = 1; j <= i; ++j) {
-			cout << arr[i][j] << " ";
+			cout << dp[i][j] << " ";
 		}
 		cout << '\n';
 	}*/
-	return 0;
-}
-int integerTriangle(int sum, int **arr) {
 	if (N == 1) {
-		return sum = arr[1][1];
+		cout << dp[1][1] << '\n';
+		return 0;
 	}
+	
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= i; ++j) {
+			int left, right;
+			if ((j - 1) == 0)
+				left = 0;
+			else
+				left = dp[i - 1][j - 1];
+			if ((i-1) < (j))
+				right = 0;
+			else
+				right = dp[i - 1][j];
+			dp[i][j] = dp[i][j] + max(left, right);
+		}
+	}
+
+	for (int j = 1; j <= N; ++j) {
+		//cout << " i : " << j << " dp[N][j] : " << dp[N][j] << '\n';
+		if (result < dp[N][j])
+			result = dp[N][j];
+	}
+	cout << result << '\n';
+	return 0;
 }
