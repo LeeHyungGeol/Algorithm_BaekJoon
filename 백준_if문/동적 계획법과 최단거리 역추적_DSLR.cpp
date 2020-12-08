9ced652f4e932cf78b84c198d36e75c1a3426326
LeeHyungGeol <rjf1138@naver.com>
2020-12-08 20:32:34 +0900

    동적 계획법, 최단거리 역추적

---
+++ 백준_if문/동적 계획법과 최단거리 역추적_DSLR.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <string.h>
+using namespace std;
+#define mod 10000
+int T, n, from, to;
+bool visited[10001]; //""0000 ~ 9999 까지의 숫자를 방문했나 안했나로 BFS를 수행""
+int D(int N);
+int S(int N);
+int L(int N);
+int R(int N);
+void bfs(int start, int end);
+
+int main() {
+	cin >> T;
+	for (int i = 0; i < T; ++i) {
+		cin >> from >> to;
+		memset(visited, false, sizeof(visited));//한 번의 TestCase 마다 memset해주기
+		bfs(from, to);
+	}
+
+	return 0;
+}
+void bfs(int start, int end) {
+	queue<pair<int, string>> q;
+	q.push({ start,"" });
+	visited[start] = true;
+	while (!q.empty()) {
+		int x = q.front().first;
+		string answer = q.front().second;
+		q.pop();
+		if (x == end) {
+			cout << answer << '\n';
+			return;
+		}
+		int d, s, l, r;
+		d = D(x);
+		if (!visited[d]) {
+			visited[d] = true;
+			q.push({ d, answer + "D" });
+		}
+
+		s = S(x);
+		if (!visited[s]) {
+			visited[s] = true;
+			q.push({ s, answer + "S" });
+		}
+
+		l = L(x);
+		if (!visited[l]) {
+			visited[l] = true;
+			q.push({ l, answer + "L" });
+		}
+
+		r = R(x);
+		if(!visited[r]) {
+			visited[r] = true;
+			q.push({ r, answer + "R" });
+		}
+	}
+}
+int D(int N) {
+	return (N * 2) % mod;//modulo 여산은 몇번을 수행해도 상관없다.
+}
+int S(int N) {
+	return N - 1 < 0 ? 9999 : N - 1;
+}
+int L(int N) {
+	return (N % 1000) * 10 + N / 1000; /////
+}
+int R(int N) {
+	return (N % 10) * 1000 + (N / 10);////
+}
--- 백준_if문/동적 계획법과 최단거리 역추적_경찰차.cpp
+++ 백준_if문/동적 계획법과 최단거리 역추적_경찰차.cpp
@@ -1,4 +1,6 @@
 //https://injae-kim.github.io/problem_solving/2020/03/11/baekjoon-2618.html
+//https://kibbomi.tistory.com/m/95
+//dp[i][j] : 1번 경찰차가 마지막으로 i, 2번 경찰차가 마지막으로 j사건을 처리했을 때, 앞으로 가야 할 이동거리'
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,27 +8,19 @@
 using namespace std;
 #define INF 1e9
 typedef long long ll;
-typedef struct police {
-	int x, y;
-}police;
 int N; //동서방향 도로의 개수, N(5≤N≤1,000)
 int W; // 처리해야 하는 사건의 개수, W(1≤W≤1,000)
 int dp[1001][1001] = { 0 };
-vector<police> p1, p2;
-int dist(int a, int b, int c, int d);
+pair<int, int> events[1001];
+int dist(const pair<int, int>& a, const pair<int, int>& b);
 int func(int a, int b);
 void backTracking(int cur1, int cur2);
 int main() {
 
-	int a, b;
 	cin >> N;
 	cin >> W;
-	p1.push_back({ 1,1 });
-	p2.push_back({ N,N });
-	for (int i = 0; i < W; ++i) {
-		cin >> a >> b;
-		p1.push_back({ a, b });
-		p2.push_back({ a, b });
+	for (int i = 1; i <= W; ++i) {
+		cin >> events[i].first >> events[i].second;
 	}
 	memset(dp, -1, sizeof(dp));
 	
@@ -41,23 +35,40 @@ int func(int cur1, int cur2) {
 	if (ret != -1)
 		return ret;
 	ret = INF;
-	int next = max(cur1, cur2) + 1;
-	int distA = dist(p1[cur1].x, p1[next].x, p1[cur1].y, p1[next].y);
-	int distB = dist(p2[cur2].x, p2[next].x, p2[cur2].y, p2[next].y);
-	int ret1 = func(next, cur2) + distA;
-	int ret2 = func(cur1, next) + distB;
-	return ret = min(ret1, ret2);
+	int next = max(cur1, cur2) + 1; //다음 사건 번호
+	int distA, distB;
+	if (cur1 == 0) 
+		distA = dist({ 1,1 }, events[next]);
+	else 
+		distA = dist(events[cur1], events[next]);
+	if (cur2 == 0) 
+		distB = dist({ N,N }, events[next]);
+	else
+		distB = dist(events[cur2], events[next]);
+
+	/*int ret1 = func(next, cur2) + distA;
+	int ret2 = func(cur1, next) + distB;*/
+	ret = min(func(next, cur2) + distA, func(cur1, next) + distB);
+	return ret;
 }
 void backTracking(int cur1, int cur2) {
 	if (cur1 == W || cur2 == W)
 		return;
 
-	int next = max(cur1, cur2) + 1;
-	int distA = dist(p1[cur1].x, p1[next].x, p1[cur1].y, p1[next].y);
-	int distB = dist(p2[cur2].x, p2[next].x, p2[cur2].y, p2[next].y);
-	int ret1 = func(next, cur2) + distA;
-	int ret2 = func(cur1, next) + distB;
-	if (ret1 < ret2) {
+	int next = max(cur1, cur2) + 1; //다음 사건 번호
+	int distA, distB;
+	if (cur1 == 0)
+		distA = dist({ 1,1 }, events[next]);
+	else
+		distA = dist(events[cur1], events[next]);
+	if (cur2 == 0)
+		distB = dist({ N,N }, events[next]);
+	else
+		distB = dist(events[cur2], events[next]);
+	/*int ret1 = func(next, cur2) + distA;
+	int ret2 = func(cur1, next) + distB;*/
+
+	if (dp[next][cur2] + distA < dp[cur1][next] + distB) {
 		cout << "1" << '\n';
 		backTracking(next, cur2);
 	}
@@ -66,7 +77,6 @@ void backTracking(int cur1, int cur2) {
 		backTracking(cur1, next);
 	}
 }
-int dist(int a, int b, int c, int d) {
-	int res = abs(a - b) + abs(c - d);
-	return res;
+int dist(const pair<int, int>& a, const pair<int, int>& b) {
+	return abs(a.first - b.first) + abs(a.second - b.second);
 }
