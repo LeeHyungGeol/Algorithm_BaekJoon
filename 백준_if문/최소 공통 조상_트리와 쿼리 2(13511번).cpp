b7ea8daf69abb5f92db11aff500744a5aec1f887
LeeHyungGeol <rjf1138@naver.com>
2021-01-17 13:26:06 +0900

    LCA(트리와 쿼리: k번째 노드)

--- 백준_if문/최소 공통 조상_트리와 쿼리 2(13511번).cpp
+++ 백준_if문/최소 공통 조상_트리와 쿼리 2(13511번).cpp
@@ -4,12 +4,15 @@
 using namespace std;
 #define LOG 21
 typedef pair<int, int> pii;
+typedef long long ll;
 int N, M;// (2 ≤ N ≤ 100,000),쿼리의 개수 M (1 ≤ M ≤ 100,000)
 int u, v, k;
+int Case;
+int lca = 0;
 int d[100001];
 bool isDepthCounted[100001];
 int parent[100001][LOG];
-int cost[100001][LOG];
+ll cost[100001][LOG];
 vector<pii> graph[100001];
 void dfs(int cur, int depth) {
 	d[cur] = depth;
@@ -28,15 +31,67 @@ void setParent() {
 	for (int i = 1; i < LOG - 1; ++i) {
 		for (int j = 1; j <= N; ++j) {
 			parent[j][i] = parent[parent[j][i - 1]][i - 1];
-			cost[j][i] = cost[cost[j][i - 1]][i - 1];
+			cost[j][i] = cost[parent[j][i - 1]][i - 1];
 		}
 	}
 }
-pii LCA(int a, int b) {
-	int sum = 0;
-
+int LCA(int a, int b) {
+	ll sum = 0;
+	if (d[a] > d[b])
+		swap(a, b);
+	for (int i = LOG - 1; i >= 0; --i) {
+		if (d[b] - d[a] >= 1 << i) {
+			sum += cost[b][i];
+			b = parent[b][i];
+		}
+	}
+	if (a == b) {
+		lca = a;
+		return sum;
+	} ////
+	for (int i = LOG - 1; i >= 0; --i) {
+		if (parent[a][i] != parent[b][i]) {
+			sum += cost[a][i];
+			sum += cost[b][i];
+			a = parent[a][i];
+			b = parent[b][i];
+		}
+	}
+	sum += parent[a][0];
+	sum += parent[b][0];
+	lca = parent[a][0];//k번째 노드를 찾기 위해서 사용할 최소 공통 조상
+	return sum;
+}
+int LCA2(int a, int b, int k) {
+	LCA(a, b);//최소 공통 조상을 찾기 위해서
+	int leftDist = d[a] - d[lca];
+	int rightDist = d[b] - d[lca];
+	if (k <= leftDist + 1) {
+		int dist = k - 1;
+		for (int i = LOG - 1; i >= 0; --i) {
+			if (dist >= 1 << i) { //if ((n & (1 << bit)) > 0) 
+				dist -= 1 << i;
+				a = parent[a][i];
+			}
+		}
+		return a;
+	}
+	else {
+		int dist = rightDist - (k - leftDist - 1);
+		for (int i = LOG - 1; i >= 0; --i) {
+			if (dist >= 1 << i) { //if ((n & (1 << bit)) > 0) 
+				dist -= 1 << i;
+				b = parent[b][i];
+			}
+		}
+		return b;
+	}
 }
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> N;
 	int from, to, dist;
 	for (int i = 1; i <= N - 1; ++i) {
@@ -44,18 +99,17 @@ int main() {
 		graph[from].push_back({ dist, to });
 		graph[to].push_back({ dist, from });
 	}
-
+	setParent();
 	cin >> M;
-	int Case;
 	for (int i = 0; i < M; ++i) {
 		cin >> Case;
 		if (Case == 1) {
 			cin >> u >> v;
-			cout << LCA(u, v).first << '\n';
+			cout << LCA(u, v) << '\n';
 		}
 		else if (Case == 2) {
 			cin >> u >> v >> k;
-			cout << LCA(u, v).second << '\n';
+			cout << LCA2(u, v, k) << '\n';
 		}
 	}
 	return 0;
