b0ae7d0ce2d7040c8d9240ccfa62c4be217796dd
LeeHyungGeol <rjf1138@naver.com>
2021-07-19 18:52:28 +0900

    2021-07-19 / 11403번 : 경로 찾기 : dfs, bfs, 플로이드워셜

--- 백준_if문/[백준 실버 1] 11403번.cpp
+++ 백준_if문/[백준 실버 1] 11403번.cpp
@@ -1,5 +1,7 @@
 // dfs, bfs 혹은 플로이드 워셜로 풀 수 있는 문제
 
+
+// 플로이드 워셜 풀이
 #include <iostream>
 #include <vector>
 
@@ -35,4 +37,59 @@ int main() {
 	}
 
 	return 0;
+}
+
+//-----------------------------------------------------------------------------------------------
+
+// dfs풀이
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> graph[100];
+vector<bool> visited;
+
+void dfs(int cur);
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			int temp;
+			cin >> temp;
+			if (temp == 1) {
+				graph[i].push_back(j);
+			}
+		}
+	}
+
+	for (int i = 0; i < N; ++i) {
+		visited.assign(N, false);
+		
+		dfs(i);
+
+		for (int j = 0; j < N; ++j) {
+			cout << visited[j] << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
+
+void dfs(int cur) {
+	// 여기에 visited를 갱신해주는 것이 아니다!
+
+	for (int next : graph[cur]) {
+		if (!visited[next]) {
+			// cur에서 next로 도달할 수 있는 것을 찾는 것이므로 if문 안에서 visited배열을 갱신한다.
+			visited[next] = true;
+			dfs(next);
+		}
+	}
+
+	return;
 }
