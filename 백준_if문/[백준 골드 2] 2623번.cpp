37c9403939d39a04d4d29b512cccd38f52220533
LeeHyungGeol <rjf1138@naver.com>
2021-07-26 15:10:13 +0900

    2021-07-26 : 2623번 : 음악 프로그램 : 위상정렬(topologicalSort)(vector.size() != N)(cycle이 발생한 경우)

--- 백준_if문/[백준 골드 2] 2623번.cpp
+++ 백준_if문/[백준 골드 2] 2623번.cpp
@@ -1,3 +1,7 @@
+// 위상정렬(topological sort) 문제
+// 우선순위 큐(priority_queue), 큐(queue)를 쓰든, 이 문제에선 아무 상관이 없다.
+// 싸이클(cycle)이 발생한 경우 == 모든 원소를 방문하기 전에 queue가 빈다 == vector.size() != N(모든 노드의 개수) 이다.
+
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -8,19 +12,67 @@ int N, M;
 vector<int> indegree;
 vector<int> graph[1001];
 
+void topologicalSort();
+
 int main() {
 	cin >> N >> M;
 
 	indegree.resize(N + 1, 0);
 
-	int temp;
+	int temp, a, b = 0;
 
 	for (int i = 0; i < M; ++i) {
-		cin >> temp;
-		for (int j = 0; j < temp; ++j) {
-			
+		cin >> temp >> a;
+
+		for (int j = 1; j < temp; ++j) {	
+			cin >> b;
+			graph[a].push_back(b);
+			++indegree[b];
+			a = b;
 		}
 	}
 
+	topologicalSort();
+
 	return 0;
+}
+
+void topologicalSort() {
+	//queue<int> q;
+	priority_queue<int> pq;
+	vector<int> answer;
+
+	for (int i = 1; i <= N; ++i) {
+		if (indegree[i] == 0) {
+			pq.push(-i);
+		}
+	}
+
+	while (!pq.empty()) {
+		int cur = -pq.top();
+		pq.pop();
+
+		answer.push_back(cur);
+
+		for (int next : graph[cur]) {
+			--indegree[next];
+
+			if (indegree[next] == 0) {
+				pq.push(-next);
+			}
+		}
+	}
+
+	// 모든 원소를 방문하여 순서를 완벽하게 정한 경우
+	if (answer.size() == N) {
+		for (int i = 0; i < answer.size(); ++i) {
+			cout << answer[i] << '\n';
+		}
+	}
+	// 모든 원소를 방문하기 전에 queue가 비는 경우 : 사이클이 발생한 경우
+	else {
+		cout << "0" << '\n';
+	}
+
+	return;
 }
