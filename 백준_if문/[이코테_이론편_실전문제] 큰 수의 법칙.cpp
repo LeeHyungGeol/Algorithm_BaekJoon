171a1fc06ba1082cdbfd6abea93063c952fd3d4d
LeeHyungGeol <rjf1138@naver.com>
2021-07-29 16:59:48 +0900

    2021-07-29 / 이코테_이론편_실전문제 : 큰 수의 법칙 : 그리디(탐욕법)

--- 백준_if문/[이코테_이론편_실전문제] 큰 수의 법칙.cpp
+++ 백준_if문/[이코테_이론편_실전문제] 큰 수의 법칙.cpp
@@ -6,27 +6,57 @@ using namespace std;
 
 int N, M, K, cnt, answer;
 vector<int> nums;
+void solution1(int first, int second);
+void solution2(int first, int second);
 
 int main() {
-	cin >> N >> M >> K;
+    cin >> N >> M >> K;
 
-	nums.resize(N, 0);
+    nums.resize(N, 0);
 
-	for (int i = 0; i < N; ++i) {
-		cin >> nums[i];
-	}
+    for (int i = 0; i < N; ++i) {
+        cin >> nums[i];
+    }
 
-	sort(nums.rbegin(), nums.rend());
+    sort(nums.rbegin(), nums.rend());
 
-	int first = nums[0], second = nums[1];
+    int first = nums[0], second = nums[1];
 
-	cnt = (M / (K + 1)) * K;
-	cnt += (M % (K + 1));
+    // solution1(first, second);
+    solution2(first, second);
 
-	answer += cnt * first;
-	answer += (M - cnt) * second;
+    cout << answer << '\n';
 
-	cout << answer << '\n';
+    return 0;
+}
 
-	return 0;
+void solution1(int first, int second) {
+    while (true) {
+        for (int i = 0; i < K; ++i) {
+            if (M == 0) {
+                break;
+            }
+            answer += first;
+            --M;
+        }
+
+        if (M == 0) {
+            break;
+        }
+
+        answer += second;
+        --M;
+    }
+
+    return;
+}
+
+void solution2(int first, int second) {
+    cnt = (M / (K + 1)) * K;
+    cnt += (M % (K + 1));
+
+    answer += cnt * first;
+    answer += (M - cnt) * second;
+
+    return;
 }
