b18898db390c071129c34eaedf4c99a2c0d141f9
LeeHyungGeol <rjf1138@naver.com>
2021-08-05 23:30:18 +0900

    2021-08-05 : 15681번 : 트리와 쿼리 : 트리, dp(다이나믹 프로그래밍), int& ret = Dp[cur];

--- 백준_if문/트리에서의 동적 계획법_트리와 쿼리(15681번).cpp
+++ 백준_if문/트리에서의 동적 계획법_트리와 쿼리(15681번).cpp
@@ -4,101 +4,63 @@
 //트리의 부모 찾기 문제(11725번)과 같이 일반적인 dfs, bfs로 돌리면 틀린다.
 //루트의 번호 R에 대한 것을 활용하자.
 //dp문제인 것을 까먹고 있었다. 메모이제이션을 하자.
+
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <stack>
+
 using namespace std;
-typedef pair<int, int> pii;
-int N;//정점의 수 N (2 ≤ N ≤ 10^5)
-int R;//루트의 번호 R ( 1 ≤ R ≤ N)
-int Q;//쿼리의 수 Q (1 ≤ Q ≤ 10^5)
-int cnt = 0;
-bool visited[100001];
-int dp[100001] = { 0 };
-vector<int> tree[100001];
-int dfs(int start);
-//void bfs(int start);
-//void countSubTreeByBfs(int start);
+
+int N, R, Q;
+vector<bool> Visited;
+vector<int> Dp;
+vector<vector<int>> Tree;
+
+int dfs(int cur);
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	cin >> N >> R >> Q;
-	int from, to;
+
+	Tree.resize(N + 1);
+	Dp.resize(N + 1, -1);
+	Visited.resize(N + 1, false);
+
 	for (int i = 0; i < N - 1; ++i) {
-		cin >> from >> to;
-		tree[from].push_back(to);
-		tree[to].push_back(from);
+		int a, b;
+		cin >> a >> b;
+		Tree[a].push_back(b);
+		Tree[b].push_back(a);
 	}
+
 	dfs(R);
-	//bfs(R);
-	int query;
+
 	for (int i = 0; i < Q; ++i) {
-		cin >> query;
-		//cnt = 0;
-		//countSubTreeByBfs(query);
-		//cout <<" cnt : "<< cnt << '\n';
-		cout << dp[query] << '\n';
+		int q;
+		cin >> q;
+		cout << Dp[q] << '\n';
 	}
 
 	return 0;
 }
+
 int dfs(int cur) {
-	if (dp[cur] != 0) return dp[cur];
-	visited[cur] = true;
-	dp[cur] = 1;
-	for (int i = 0; i < tree[cur].size(); ++i) {
-		int next = tree[cur][i];
-		if (!visited[next]) {
-			visited[next] = true;
-			dp[cur] += dfs(next);
+	int& ret = Dp[cur];
+
+	if (ret != -1) {
+		return ret;
+	}
+
+	ret = 1;
+	Visited[cur] = true;
+
+	for (int next : Tree[cur]) {
+		if (!Visited[next]) {
+			ret += dfs(next);
 		}
 	}
-	return dp[cur];
+
+	return ret;
 }
-//void bfs(int start) {
-//	fill(visited, visited + 100001, false);
-//	queue<int> q;
-//	q.push(start);
-//	visited[start] = true;
-//
-//	while (!q.empty()) {
-//		int cur = q.front();
-//
-//		q.pop();
-//		for (int i = 0; i < tree[cur].size(); ++i) {
-//			int next = tree[cur][i];
-//			
-//			if (!visited[next]) {
-//				parent[next] = cur;
-//				visited[next] = true;
-//				q.push(next);
-//			}
-//		}
-//	}
-//}
-//void countSubTreeByBfs(int start) {
-//	fill(visited, visited + 100001, false);
-//	queue<int> q;
-//	q.push(start);
-//	visited[start] = true;
-//	cnt++;
-//	while (!q.empty()) {
-//		int cur = q.front();
-//
-//		q.pop();
-//		for (int i = 0; i < tree[cur].size(); ++i) {
-//			int next = tree[cur][i];
-//			//cout << next << ' ';
-//			if (parent[next] == start)
-//				continue;
-//			if (!visited[next]) {
-//				cnt++;				
-//				visited[next] = true;
-//				q.push(next);
-//			}
-//		}
-//	}
-//}
