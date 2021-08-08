dca361795f3351770afbced4c788c4f5fabc6cb4
LeeHyungGeol <rjf1138@naver.com>
2021-08-08 23:51:38 +0900

    2021-08-08 / 11509번 : 풍선 맞추기 : 그리디(탐욕법), 배열의 index를 입력값으로 하기(++Heights[h-1];)

--- 백준_if문/[백준 실버 1] 11509번.cpp
+++ 백준_if문/[백준 실버 1] 11509번.cpp
@@ -1,3 +1,9 @@
+// 그리디(탐욕법) 문제
+// 입력 받는 N의 크기 만큼 배열을 선언하여, 
+// 차례대로 수를 배열에 입력 받으면서, 배열의 인덱스(index)를 입력값이라고 생각하고
+// 배열의 값이 0이면(Heights[h] == 0), ++Heights[h-1]을 해주고(height가 1 작은 화살 추가), ++Answer
+// 배열의 값이 존재한다면, --Heights[h], 를 해주고, ++Heights[h-1]를 해준다.
+
 #include <iostream>
 #include <vector>
 
