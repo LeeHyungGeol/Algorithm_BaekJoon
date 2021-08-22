03527f2b67c51cf631d9a01b0e74cdf746774652
LeeHyugGeol <rjf1138@naver.com>
2021-08-22 20:46:53 +0900

    2021-08-22 / 16432번 : 떡장수와 호랑이 : dfs, 답이 여러개인 경우, flag, 반환형 bool 활용

--- 백준_if문/[백준 골드 5] 16432번.cpp
+++ 백준_if문/[백준 골드 5] 16432번.cpp
@@ -1,9 +1,16 @@
+// dfs문제
+
+// 항상 범위에 주의하자!!
+// 답을 찾은 경우에 flag 혹은 dfs함수의 반환형을 bool로 하여,
+// 답이 아닌 경우에 대하여 처리를 잘하자.
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 int N;
+bool Flag = false;
 vector<int> Answer;
 vector<vector<int>> Graph;
 vector<vector<bool>> Visited;
@@ -14,68 +21,62 @@ void dfs(int index, int cur);
 int main() {
 	cin >> N;
 
-	Graph.resize(N);
-	Visited.resize(N, vector<bool>(10, false));
+	Answer.resize(N + 1, 0);
+	Graph.resize(N+1, vector<int>(10, 0));
+	Visited.resize(N+2, vector<bool>(10, false));
 
 	for (int i = 0; i < N; ++i) {
 		int size;
 		cin >> size;
+
 		for (int j = 0; j < size; ++j) {
 			int num;
 			cin >> num;
-			Graph[i].push_back(num);
+			Graph[i][num] = 1;
 		}
 	}
 
 	dfsAll();
-
-	if ((int)Answer.size() < N) {
+	
+	if(!Flag) {
 		cout << "-1" << '\n';
-		return 0;
-	}
-
-	for (int i = 0; i < Answer.size(); ++i) {
-		cout << Answer[i] << '\n';
 	}
 
 	return 0;
 }
-
 void dfsAll() {
-	for (int i = 0; i < Graph[0].size(); ++i) {
-		int start = Graph[0][i];
-
-		if (!Visited[0][start]) {
+	for (int i = 1; i <= 9; ++i) {
+		if (Graph[0][i] == 1 && !Flag) {
+			Visited[0][i] = true;
 			Answer.clear();
-
-			dfs(0, start);
-			
-			if (Answer.size() == N) {
-				break;
-			}
+			dfs(0, i);
 		}
 	}
 	return;
 }
 
 void dfs(int index, int cur) {
+	if (Flag) {
+		return;
+	}
 	if (index == N) {
+		for (int i = 0; i < Answer.size(); ++i) {
+			cout << Answer[i] << '\n';
+		}
+		Flag = true;
 		return;
 	}
 
-	Answer.push_back(cur);
-
-	if ((int)Answer.size() == N) {
-		return;
-	}
-
-	Visited[index][cur] = true;
-	
-	for (int i = 0; i < Graph[index + 1].size(); ++i) {
-		int next = Graph[index + 1][i];
-
-		if (next != cur && !Visited[index + 1][next]) {
-			dfs(next, index + 1);
+	for (int next = 1; next <= 9; ++next) {
+		if (cur != next && Graph[index][cur] == 1 && !Visited[index + 1][next]) {
+			Visited[index + 1][next] = true;
+			Answer.push_back(cur);
+			dfs(index + 1, next);
+			Answer.pop_back();
+			
+			if (Flag) {
+				return;
+			}
 		}
 	}
 }
