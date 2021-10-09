e2571b6b88ea0c28351f09a12e720b568c030d5d
LeeHyungGeol <rjf1138@naver.com>
2021-10-09 23:56:23 +0900

    2021-10-09 / 17406번 : 배열돌리기4 : 구현, 배열, 브루트포스(완전탐색)

---
+++ 백준_if문/[백준 골드 4] 17406번.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int N, M, K, Answer = INT_MAX;
+// µ¿, ³², ¼­, ºÏ
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
+vector<bool> Select;
+vector<vector<int>> Arr;
+vector<vector<int>> Query;
+
+void dfs(int cnt, vector<int>& order);
+void rotate(int q);
+int turn(int startX, int startY, int endX, int endY, int x, int y, int dir);
+
+int main() {
+	cin >> N >> M >> K;
+
+	Arr.resize(N + 1, vector<int>(M + 1, 0));
+	Select.resize(K, false);
+	Query.resize(K, vector<int>(3, 0));
+
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= M; ++j) {
+			cin >> Arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < K; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			cin >> Query[i][j];
+		}
+	}
+
+	vector<int> order;
+
+	dfs(0, order);
+
+	cout << Answer << '\n';
+
+	return 0;
+}
+
+void dfs(int cnt, vector<int>& order) {
+	if (cnt == K) {
+		for (int queryIndex : order) {
+			rotate(queryIndex);
+		}
+		for (int i = 1; i <= N; ++i) {
+			int sum = 0;
+			for (int j = 1; j <= N; ++j) {
+				sum += Arr[i][j];
+			}
+			Answer = min(Answer, sum);
+		}
+		return;
+	}
+
+	for (int i = 0; i < K; ++i) {
+		if (Select[i]) {
+			continue;
+		}
+		order.push_back(i);
+		Select[i] = true;
+		dfs(cnt + 1, order);
+		Select[i] = false;
+		order.pop_back();
+	}
+}
+
+void rotate(int q) {
+	int x = 0, y = 0, dir = 0, swapTemp = 0;
+	int startX = Query[q][0]-Query[q][2], startY = Query[q][1] - Query[q][2], endX = Query[q][0] + Query[q][2], endY = Query[q][1] + Query[q][2];
+	int count = (abs(endX - endY) + 1) / 2;
+
+	while (count--) {
+		swapTemp = Arr[startX][startY];
+		x = startX, y = startY, dir = 0;
+
+		while (true) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+
+			int temp = Arr[nx][ny];
+			swap(swapTemp, Arr[nx][ny]);
+			swapTemp = temp;
+
+			x = nx;
+			y = ny;
+
+			dir = turn(startX, startY, endX, endY, x, y, dir);
+
+			if (x == startX && y == startY) {
+				break;
+			}
+		}
+		++startX;
+		++startY;
+		--endX;
+		--endY;
+	}
+	return;
+}
+
+int turn(int startX, int startY, int endX, int endY, int x, int y, int dir) {
+	if (x == startX && y == endY) {
+		dir = 1;
+	}
+	else if (x == endX && y == endY) {
+		return 2;
+	}
+	else if (x == endX && y == startY) {
+		return 3;
+	}
+	return dir;
+}
