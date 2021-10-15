aa9d25df5eff412e337b6aa46a4d9283ddbeebdc
LeeHyungGeol <rjf1138@naver.com>
2021-10-16 02:52:19 +0900

    2021-10-16 / 2753번 : 윤년 : 구현, 수학

---
+++ 백준_if문/[백준 브론즈 4] 2753번.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+	cin >> N;
+
+	if ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0) {
+		cout << "1" << '\n';
+	}
+	else {
+		cout << "0" << '\n';
+	}
+	return 0;
+}
