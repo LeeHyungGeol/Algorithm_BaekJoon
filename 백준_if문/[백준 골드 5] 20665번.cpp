41cf1977e85f0c07c072b8896f6638ce8ccd9722
LeeHyungGeol <rjf1138@naver.com>
2021-10-17 17:31:20 +0900

    2021-10-17 / 9081번 : 단어 맞추기 : 구현, 순열,조합, next_per

---
+++ 백준_if문/[백준 골드 5] 20665번.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+typedef pair<int, int> pii;
+
+using namespace std;
+
+int N, T, P;
+string Start, End;
+vector<pii> Times;
+vector<vector<int>> Arr;
+
+int main() {
+	cin >> N >> T >> P;
+
+	Arr.resize(N + 1, vector<int>(721, 0));
+
+	for (int t = 0; t < T; ++t) {
+		
+	}
+
+	return 0;
+}
+
+int timeToMinutes(string time) {
+}
--- 백준_if문/[백준 골드 5] 9081번.cpp
+++ 백준_if문/[백준 골드 5] 9081번.cpp
@@ -30,3 +30,66 @@ int main() {
 
 	return 0;
 }
+
+
+//-----------2번째 풀이(순열 구현)--시간초과----------------//
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int T;
+string Str;
+vector<bool> Select;
+set<string> Ranks;
+
+void dfs(string str, int cnt);
+
+int main() {
+	cin >> T;
+
+	for (int t = 0; t < T; ++t) {
+		cin >> Str;
+		
+		Ranks.clear();
+		Select.assign(Str.length(), false);
+
+		dfs("", 0);
+
+		for (auto it = Ranks.begin(); it != Ranks.end(); ++it) {
+			if (*it == Str) {
+				++it;
+				if (it == Ranks.end()) {
+					cout << Str << '\n';
+				}
+				else {
+					cout << *it << '\n';
+				}
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+void dfs(string str, int cnt) {
+	if (cnt == Str.length()) {
+		Ranks.insert(str);
+		return;
+	}
+
+	for (int i = 0; i < Select.size(); ++i) {
+		if (Select[i]) {
+			continue;
+		}
+		Select[i] = true;
+		str.push_back(Str[i]);
+		dfs(str, cnt + 1);
+		str.pop_back();
+		Select[i] = false;
+	}
+}
