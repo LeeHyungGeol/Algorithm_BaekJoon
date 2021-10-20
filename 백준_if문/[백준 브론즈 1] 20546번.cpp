08751b14413afa967f03a4cf25510eb5d8132f46
LeeHyungGeol <rjf1138@naver.com>
2021-10-21 02:30:06 +0900

    2021-10-21 / 20546번 : 기적의매매법 : 구현

---
+++ 백준_if문/[백준 브론즈 1] 20546번.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct Node {
+	int cash, stocks;
+};
+
+int Money;
+vector<int> Arr;
+
+int main() {
+	cin >> Money;
+
+	for (int i = 0; i < 14; ++i) {
+		int stock;
+		cin >> stock;
+		Arr.push_back(stock);
+	}
+
+	Node junHyeon = { Money, 0 }, seongMin = {Money, 0};
+
+	for (int i = 0; i < 14; ++i) {
+		int nowStock = junHyeon.cash / Arr[i];
+		if (nowStock) {
+			junHyeon.cash -= (nowStock * Arr[i]);
+			junHyeon.stocks += nowStock;
+		}
+	}
+
+	int before = Arr[0], up = 0, down = 0;
+
+	for (int i = 0; i < 14; ++i) {
+		int cur = Arr[i];
+		int nowStock = seongMin.cash / Arr[i];
+		if (up == 3) {
+			seongMin.cash += seongMin.stocks * Arr[i];
+			seongMin.stocks = 0;
+			up = 0;
+			continue;
+		}
+		else if (down == 3) {
+			seongMin.cash -= (nowStock * Arr[i]);
+			seongMin.stocks += nowStock;
+			down = 0;
+			continue;
+		}
+		
+		if (cur > before) {
+			++up;
+		}
+		else if (cur < before) {
+			++down;
+		}
+		else if (cur == before) {
+			up = 0;
+			down = 0;
+		}
+		before = cur;
+	}
+
+	if ((junHyeon.cash + junHyeon.stocks * Arr[13]) > (seongMin.cash + seongMin.stocks * Arr[13])) {
+		cout << "BNP" << '\n';
+	}
+	else if ((junHyeon.cash + junHyeon.stocks * Arr[13]) < (seongMin.cash + seongMin.stocks * Arr[13])) {
+		cout << "TIMING" << '\n';
+	}
+	else {
+		cout << "SAMESAME" << '\n';
+	}
+	return 0;
+}
