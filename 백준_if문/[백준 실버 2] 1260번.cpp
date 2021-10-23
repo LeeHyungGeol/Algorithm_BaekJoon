25ee438cd81a4d84bc2841c571f73ac5ee0a493c
LeeHyungGeol <rjf1138@naver.com>
2021-10-24 02:46:49 +0900

    2021-10-24 / 1260번 : DFS와 BFS : dfs,bfs

--- 백준_if문/[백준 실버 2] 1260번.cpp
+++ 백준_if문/[백준 실버 2] 1260번.cpp
@@ -1,6 +1,3 @@
-// bfs, dfs 문제
-// graph 탐색을 물어보는 기본 문제
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -9,8 +6,8 @@
 using namespace std;
 
 int N, M, V;
-vector<vector<int>> graph;
-vector<bool> visited;
+vector<vector<int>> Graph;
+vector<bool> Visited;
 
 void dfs(int cur);
 void bfs(int cur);
@@ -18,60 +15,53 @@ void bfs(int cur);
 int main() {
 	cin >> N >> M >> V;
 
-	graph.resize(N+1, vector<int>());
-	visited.resize(N+1, false);
+	Graph.resize(N+1);
 
 	for (int i = 0; i < M; ++i) {
 		int a, b;
 		cin >> a >> b;
-		graph[a].push_back(b);
-		graph[b].push_back(a);
+		Graph[a].push_back(b);
+		Graph[b].push_back(a);
 	}
 
-	for (int i = 1; i < graph.size(); ++i) {
-		sort(graph[i].begin(), graph[i].end());
-	}
+	for (int i = 1; i <= N; ++i) sort(Graph[i].begin(), Graph[i].end());
 
+	Visited.assign(N + 1, false);
 	dfs(V);
 	cout << '\n';
+	Visited.assign(N + 1, false);
 	bfs(V);
 
 	return 0;
 }
 
 void dfs(int cur) {
-	visited[cur] = true;
-
+	Visited[cur] = true;
 	cout << cur << ' ';
 
-	for (int next : graph[cur]) {
-		if (!visited[next]) {
-			dfs(next);
-		}
+	for (int next : Graph[cur]) {
+		if (Visited[next]) continue;
+		dfs(next);
 	}
 }
 
 void bfs(int cur) {
-	visited.assign(N + 1, false);
 	queue<int> q;
+
 	q.push(cur);
-	visited[cur] = true;
+	Visited[cur] = true;
 
 	while (!q.empty()) {
-		int now = q.front();
+		int cur = q.front();
 		q.pop();
 
-		cout << now << ' ';
+		cout << cur << ' ';
 
-		for (int next : graph[now]) {
-			if (!visited[next]) {
-				visited[next] = true;
-				q.push(next);
-			}
+		for (int next : Graph[cur]) {
+			if (Visited[next]) continue;
+			Visited[next] = true;
+			q.push(next);
 		}
 	}
-
-	cout << '\n';
-
-	return;
 }
+
