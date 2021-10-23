0ee8c8c12973e9cbcf1207031632290d9d544b5f
LeeHyungGeol <rjf1138@naver.com>
2021-10-23 21:10:18 +0900

    2021-10-23 / 17135번 : 캐슬디펜스 : 구현,시뮬,조합,완전탐색

--- 백준_if문/[백준 골드 4] 17135번.cpp
+++ 백준_if문/[백준 골드 4] 17135번.cpp
@@ -5,17 +5,26 @@
 
 using namespace std;
 
-typedef struct Node {
-	int x, y;
+typedef struct Enemy {
+	int x, y, dist;
 }Node;
+typedef pair<int, int> pii;
 
 int N, M, D, Answer;
 vector<vector<int>> Arr;
-vector<Node> Archers;
+vector<vector<int>> TempArr;
+vector<vector<bool>> Visited;
+vector<pii> Archers;
+vector<pii> Enemies;
 vector<bool> Select;
 
 void dfs(int startIndex, int cnt);
-void playCastleDefense();
+int playCastleDefense();
+bool isFinish();
+int shootEnemies();
+void moveEnemies();
+int getDist(int x, int y, int nx, int ny);
+bool compare(const Enemy& a, const Enemy& b);
 
 int main() {
 	cin >> N >> M >> D;
@@ -31,6 +40,8 @@ int main() {
 
 	dfs(0, 0);
 	
+	cout << Answer << '\n';
+
 	return 0;
 }
 
@@ -39,10 +50,11 @@ void dfs(int startIndex, int cnt) {
 		Archers.clear();
 		for (int i = 0; i < Select.size(); ++i) {
 			if (Select[i]) {
-				Archers.push_back({ N + 1,i });
+				Archers.push_back({ N,i });
 			}
 		}
-		playCastleDefense();
+		int result = playCastleDefense();
+		Answer = max(Answer, result);
 		return;
 	}
 
@@ -54,8 +66,102 @@ void dfs(int startIndex, int cnt) {
 	}
 }
 
-void playCastleDefense() {
+int playCastleDefense() {
+	int result = 0;
+	TempArr = Arr;
+
 	while (true) {
-		
+		Enemies.clear();
+		Visited.assign(N, vector<bool>(M, false));
+
+		if (isFinish()) break;
+		result += shootEnemies();
+		moveEnemies();
 	}
+
+	return result;
+}
+
+bool isFinish() {
+	bool flag = true;
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			if (TempArr[i][j] == 1) {
+				flag = false;
+				Enemies.push_back({ i,j });
+			}
+		}
+	}
+
+	return flag;
+}
+
+int shootEnemies() {
+	vector<pii> deads;
+	int result = 0;
+
+	for (pii archer : Archers) {
+		int x = archer.first;
+		int y = archer.second;
+		vector<Enemy> candidates;
+
+		for (pii enemy : Enemies) {
+			int nx = enemy.first;
+			int ny = enemy.second;
+			int dist = getDist(x, y, nx, ny);
+
+			if (dist <= D) {
+				candidates.push_back({ nx,ny,dist });
+			}
+		}
+		
+		int size = candidates.size();
+
+		if (size > 1) {
+			sort(candidates.begin(), candidates.end(), compare);
+			deads.push_back({ candidates.front().x, candidates.front().y });
+		}
+		else if (size == 1) {
+			deads.push_back({ candidates.front().x, candidates.front().y });
+		}
+	}
+
+	for (pii dead : deads) {
+		int x = dead.first;
+		int y = dead.second;
+
+		if (!Visited[x][y]) {
+			Visited[x][y] = true;
+			TempArr[x][y] = 0;
+			++result;
+		}
+	}
+
+	return result;
+}
+
+void moveEnemies() {
+	for (int i = Enemies.size() - 1; i >= 0; --i) {
+		int x = Enemies[i].first;
+		int y = Enemies[i].second;
+
+		if (TempArr[x][y] == 0) continue;
+		if (x == N - 1) TempArr[x][y] = 0;
+		else {
+			TempArr[x][y] = 0;
+			TempArr[x + 1][y] = 1;
+		}
+	}
+}
+
+int getDist(int x, int y, int nx, int ny) {
+	return abs(x - nx) + abs(y - ny);
+}
+
+bool compare(const Enemy& a, const Enemy& b) {
+	if (a.dist == b.dist) {
+		return a.y < b.y;
+	}
+	return a.dist < b.dist;
 }
