6afd9ea38d8a7e0f9d9e8f3cf1bcff8bb99d0f67
LeeHyungGeol <rjf1138@naver.com>
2021-10-27 01:17:40 +0900

    2021-10-27 / 17140번 : 이차원 배열과 연산 : 구현,시뮬

--- 백준_if문/[백준 골드 4] 17140번.cpp
+++ 백준_if문/[백준 골드 4] 17140번.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 typedef pair<int, int> pii;
 
-int r, c, k, Answer;
-vector<vector<int>> Arr;
-vector<vector<pii>> TempArr;
-map<int, int> Counter;
+int r, c, k, Answer, Row = 3, Col = 3;
+int Arr[101][101];
+int Counter[101];
 
 void operateR();
 void operateC();
@@ -20,22 +19,20 @@ bool compare(const pii& a, const pii& b);
 int main() {
 	cin >> r >> c >> k;
 
-	Arr.assign(3, vector<int>(3, 0));
-
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 3; ++j) {
+	for (int i = 1; i <= 3; ++i) {
+		for (int j = 1; j <= 3; ++j) {
 			cin >> Arr[i][j];
 		}
 	}
 
 	bool flag = false;
 
-	for (int i = 0; i < 100; ++i) {
-		if (Arr[r - 1][c - 1] == k) {
+	for (int i = 0; i < 101; ++i) {
+		if (Arr[r][c] == k) {
 			flag = true;
 			break;
 		}
-		if (Arr.size() >= Arr[0].size()) {
+		if (Row >= Col) {
 			operateR();
 		}
 		else {
@@ -55,65 +52,81 @@ int main() {
 }
 
 void operateR() {
-	int longest = 0;
-	TempArr.clear();
+	vector<int> longest;
 
-	for (int i = 0; i < Arr.size(); ++i) {
-		Counter.clear();
+	for (int i = 1; i <= Row; ++i) {
 		vector<pii> temp;
+		memset(Counter, 0, sizeof(Counter));
 
-		for (int j = 0; j < Arr[i].size(); ++j) {
-			if (Arr[i][j] == 0) continue;
-			++Counter[Arr[i][j]];
+		for (int j = 1; j <= Col; ++j) ++Counter[Arr[i][j]];
+
+		for (int j = 1; j < 101; ++j) {
+			if (Counter[j] == 0) continue;
+			temp.push_back({ j, Counter[j] });
 		}
 
-		temp.assign(Counter.begin(), Counter.end());
 		sort(temp.begin(), temp.end(), compare);
 
-		TempArr.push_back(temp);
-		if (temp.size() > longest) {
-			longest = temp.size();
-		}
-	}
+		for (int j = 1; j <= Col; ++j) Arr[i][j] = 0;
 
-	Arr.clear();
-
-	if (longest > 50) longest = 50;
-
-	for (int i = 0; i < TempArr.size(); ++i) {
-		vector<int> temp(longest * 2, 0);
-		int k = 0;
-
-		for (int j = 0; j < TempArr[i].size(); ++j) {
-			temp[k] = TempArr[i][j].first;
-			temp[k + 1] = TempArr[i][j].second;
+		int k = 1;
+		for (int j = 0; j < temp.size(); ++j) {
+			Arr[i][k] = temp[j].first;
+			Arr[i][k + 1] = temp[j].second;
 			k += 2;
 		}
-
-		Arr.push_back(temp);
+		--k;
+		longest.push_back(k);
 	}
+
+	sort(longest.begin(), longest.end());
+	Col = longest.back();
 }
 
 void operateC() {
-	rotate();
+	vector<int> longest;
 
-	operateR();
+	for (int i = 1; i <= Col; ++i) {
+		vector<pii> temp;
+		memset(Counter, 0, sizeof(Counter));
 
-	rotate();
+		for (int j = 1; j <= Row; ++j) ++Counter[Arr[j][i]];
+
+		for (int j = 1; j < 101; ++j) {
+			if (Counter[j] == 0) continue;
+			temp.push_back({ j, Counter[j] });
+		}
+
+		sort(temp.begin(), temp.end(), compare);
+
+		for (int j = 1; j <= Row; ++j) Arr[j][i] = 0;
+
+		int k = 1;
+		for (int j = 0; j < temp.size(); ++j) {
+			Arr[k][i] = temp[j].first;
+			Arr[k+1][i] = temp[j].second;
+			k += 2;
+		}
+		--k;
+		longest.push_back(k);
+	}
+
+	sort(longest.begin(), longest.end());
+	Row = longest.back();
 }
 
 // y=x ´ëÄªÀÌµ¿
-void rotate() {
-	vector<vector<int>> temp(Arr[0].size(), vector<int>(Arr.size(), 0));
-
-	for (int i = 0; i < Arr.size(); ++i) {
-		for (int j = 0; j < Arr[i].size(); ++j) {
-			temp[j][i] = Arr[i][j];
-		}
-	}
-
-	Arr = temp;
-}
+//void rotate() {
+//	vector<vector<int>> temp(Arr[0].size(), vector<int>(Arr.size(), 0));
+//
+//	for (int i = 0; i < Arr.size(); ++i) {
+//		for (int j = 0; j < Arr[i].size(); ++j) {
+//			temp[j][i] = Arr[i][j];
+//		}
+//	}
+//
+//	Arr = temp;
+//}
 
 bool compare(const pii& a, const pii& b) {
 	if (a.second == b.second) {
