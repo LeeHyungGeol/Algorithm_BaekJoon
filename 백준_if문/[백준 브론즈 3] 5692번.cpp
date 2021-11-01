5db3bca30f376a528b1f26038df63588284fb992
LeeHyungGeol <rjf1138@naver.com>
2021-11-02 02:06:09 +0900

    2021-11-02 / 5692번 : 팩토리얼 진법 : 수학, 팩토리얼

--- 백준_if문/[백준 브론즈 3] 5692번.cpp
+++ 백준_if문/[백준 브론즈 3] 5692번.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int main() {
+int factorial(int n);
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+
+	while (cin >> n) {
+		if (n == 0) break;
+		int i = 1, result = 0;
+		while (n) {
+			int temp = n % 10;
+			n /= 10;
+			result += (temp * factorial(i));
+			++i;
+		}
+		cout << result << '\n';
+	}
 	return 0;
+}
+
+int factorial(int n) {
+	if (n == 0 || n == 1) return 1;
+	return n * factorial(n - 1);
 }
