e09e270b7f9ab61d5ede323a4af1c5bfd6af06d7
LeeHyungGeol <rjf1138@naver.com>
2021-11-10 00:28:30 +0900

    2021-11-10 / 17413번 : 단어 뒤집기 2 : 구현,문자열

--- 백준_if문/[백준 실버 3] 17413번.cpp
+++ 백준_if문/[백준 실버 3] 17413번.cpp
@@ -24,11 +24,6 @@ int main() {
 			continue;
 		}
 		else if (S[i] == '>') {
-			if (!str.empty()) {
-				reverse(str.begin(), str.end());
-				cout << str;
-				str.clear();
-			}
 			cout << S[i];
 			flag = false;
 			continue;
