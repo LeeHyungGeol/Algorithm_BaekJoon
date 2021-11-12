93a6cfd3fd61a7f2c97de373e014c29f2cd208a8
LeeHyungGeol <rjf1138@naver.com>
2021-11-13 01:06:46 +0900

    2021-11-13 / 17276번 : 배열 돌리기 : 배열 45도 회전, 구현

--- 백준_if문/[백준 실버 1] 17276번.cpp
+++ 백준_if문/[백준 실버 1] 17276번.cpp
@@ -1,22 +1,103 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int N;
+int T, N, D;
 vector<vector<int>> Arr;
-vector<int> Temp;
+
+void rotate(int deg);
+void clockwise();
+void counterclockwise();
 
 int main() {
-	cin >> N;
+	cin >> T;
 
-	Arr.assign(N + 1, vector<int>(N + 1, 0));
-	Temp.assign(N + 1, 0);
+	for (int t = 0; t < T; ++t) {
+		cin >> N >> D;
 
-	rotateLeftDiagnol();
-	rotateCenterRow();
-	rotateRightDiagnol();
-	rotateCenterColumn();
+		Arr.assign(N + 1, vector<int>(N + 1, 0));
+		
+		for (int i = 1; i <= N; ++i) {
+			for (int j = 1; j <= N; ++j) {
+				cin >> Arr[i][j];
+			}
+		}
+
+		rotate(D);
+
+		for (int i = 1; i <= N; ++i) {
+			for (int j = 1; j <= N; ++j) {
+				cout << Arr[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+	}
 
 	return 0;
+}
+
+void rotate(int deg) {
+	int time = 0;
+
+	if (deg > 0) {
+		time = (deg / 45) % 8;
+
+		for (int t = 0; t < time; ++t) {
+			clockwise();
+		}
+	}
+	else {
+		deg = abs(deg);
+		time = (deg / 45) % 8;
+
+		for (int t = 0; t < time; ++t) {
+			counterclockwise();
+		}
+	}
+}
+
+void clockwise() {
+	vector<vector<int>> temp = Arr;
+	
+	for(int i = 0; i < N; ++i) {
+		temp[1+i][N-i] = Arr[1+i][(N+1)/2];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[(N+1)/2][N-i] = Arr[1 + i][N - i];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[N-i][N-i] = Arr[(N + 1) / 2][N - i];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[N-i][(N+1)/2] = Arr[N - i][N - i];
+	}
+
+	Arr = temp;
+}
+
+void counterclockwise() {
+	vector<vector<int>> temp = Arr;
+
+	for (int i = 1; i <= N; ++i) {
+		temp[(N+1)/2][i] = Arr[i][i];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[N-i][1+i] = Arr[(N + 1) / 2][i+1];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[N-i][(N+1)/2] = Arr[N-i][1+i];
+	}
+
+	for (int i = 0; i < N; ++i) {
+		temp[N-i][N-i] = Arr[N - i][(N + 1) / 2];
+	}
+
+	Arr = temp;
 }
