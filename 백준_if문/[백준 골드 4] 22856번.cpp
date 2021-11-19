0ca72511d63faa43db68a88f0914c961abf2fac7
LeeHyungGeol <rjf1138@naver.com>
2021-11-19 23:43:31 +0900

    2021-11-19 / 22856번 : 트리 순회 : 💡이진트리순회, 구현

---
+++ 백준_if문/[백준 골드 4] 22856번.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef struct Node {
+	int num;
+	Node* left;
+	Node* right;
+}Node;
+int N;
+vector<Node> Nodes;
+vector<bool> Visited;
+
+int main() {
+
+	
+	return 0;
+}
